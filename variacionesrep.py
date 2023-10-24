a75821713bdab4c7b112008ca31104466eacc0f7
ALVARO GARABAL <alvgarabal9@gmail.com>
2023-10-24 23:11:04 +0200

    ejercicio 1 y 2 completos

--- variacionesrep.py
+++ variacionesrep.py
@@ -16,12 +16,34 @@ def variacionesRepeticion(elementos, cantidad):
                 
     yield from backtracking([])
 
+def combinaciones(elementos, cantidad):
+    
+    def backtracking(sol,i):
+        if len(sol) == cantidad:
+            yield sol.copy()
+        else:
+            #ramificamos con elementos posteriores
+            for opcion in elementos[i:]:
+                #condición de prometedor
+                if opcion not in sol:
+                    #obtencion de ínidice en la lista de elementos
+                    indice = elementos.index(opcion)
+                    sol.append(opcion)
+                    yield from backtracking(sol,indice)
+                    sol.pop()
+                
+    yield from backtracking([],0)
+
 # COMPLETAR las actividades 1 y 2 para permutaciones y combinaciones
     
 if __name__ == "__main__":
     for n in (1,2,3):
         print('Variaciones con repeticion n =',n)
         for x in variacionesRepeticion(['tomate','queso','anchoas'],n):
+            print(x)
+
+        print('Combinaciones con combinacion n =',3)
+        for x in combinaciones(['tomate','queso','anchoas','aceitunas'],3):
             print(x)
 
     # probar las actividades 1 y 2 para permutaciones y combinaciones
